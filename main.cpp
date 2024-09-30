56b90a5e4a3ae7a5a5fe3ffcd2df756c7555cdd1
chooyy <choo-yy@qq.com>
2024-09-30 10:17:27 +0800

    feat：连续输出渲染图片

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <string>
 #include <vector>
 
 
@@ -14,6 +15,7 @@ VTK_MODULE_INIT(vtkInteractionStyle);
 #include <vtkRenderWindow.h>  
 #include <vtkRenderWindowInteractor.h>  
 #include <vtkProperty.h>  
+#include <vtkLight.h>
 #include <vtkCamera.h>  
 #include <vtkAxesActor.h>
 #include <vtkPNGWriter.h>  
@@ -37,7 +39,7 @@ int main(int argc, char* argv[])
     int                   height     = 480;                          // 图像高度
     double                f          = 525.0;                        // 焦距
     double                view_angle = 2.0 * atan2(height / 2.0, f); // 垂直视场角
-    std::array<double, 3> cam_pos    = {200, 0, 0};                  // 相机位置
+    std::array<double, 3> cam_pos    = {0, 0, -200};                 // 相机位置
 
     float l = 25.; // 小球距离坐标原点的距离
     std::array<std::array<float, 7>, 7> spheres {
@@ -83,8 +85,15 @@ int main(int argc, char* argv[])
     // 获取并设置相机的位置和朝向
     vtkCamera* camera = renderer->GetActiveCamera();
     camera->SetViewAngle(view_angle * 180.0 / vtkMath::Pi()); // 设置垂直视场角
-    camera->SetViewUp(0, 0, 1);                               // 设置相机的上方向向量
+    camera->SetViewUp(0, -1, 0);                              // 设置相机的上方向向量
     camera->SetPosition(cam_pos[0], cam_pos[1], cam_pos[2]);  // 设置相机位置
+
+    // // 创建光源
+    // vtkLight* light = vtkLight::New();
+    // light->SetFocalPoint(0, 0, 0);
+    // light->SetPosition(1.2 * cam_pos[0], 1.2 * cam_pos[1], 1.2 * cam_pos[2]);
+    // light->SetColor(1, 1, 1); // 设置光源颜色为白色
+    // renderer->AddLight(light);
 
     // 创建一个渲染窗口
     vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
@@ -94,21 +103,24 @@ int main(int argc, char* argv[])
     // vtkMyCallback* mo1 = vtkMyCallback::New();
     // renderer->AddObserver(vtkCommand::StartEvent, mo1);
 
-    // for (size_t i = 0; i < 360; i++) {
-    //     renderWindow->Render();
-    //     renderer->GetActiveCamera()->Azimuth(1);
-    // }
-
     // 创建WindowToImageFilter
     vtkSmartPointer<vtkWindowToImageFilter> windowToImageFilter = vtkSmartPointer<vtkWindowToImageFilter>::New();
     windowToImageFilter->SetInput(renderWindow);
-    windowToImageFilter->ReadFrontBufferOff(); // 从后缓冲区读取，以避免闪烁
     windowToImageFilter->Update();
     // 创建writer，这里以PNG格式为例
     vtkSmartPointer<vtkPNGWriter> writer = vtkSmartPointer<vtkPNGWriter>::New();
-    writer->SetFileName("output.png");                                // 设置输出文件名
     writer->SetInputConnection(windowToImageFilter->GetOutputPort()); // 设置输入连接
-    writer->Write();                                                  // 写入文件
+
+    camera->Elevation(45);
+    for (size_t i = 0; i < 360; i++) {
+        camera->Azimuth(1);
+        renderWindow->Render();
+
+        static int idx = 0;
+        windowToImageFilter->Modified();                                        // 更新输出图片显示，如果不调用这个输出图片不会变化
+        writer->SetFileName(("img/" + std::to_string(idx++) + ".png").c_str()); // 设置输出文件名output.png
+        writer->Write();                                                        // 写入文件
+    }
 
     // 创建一个渲染窗口交互器
     vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
